d0a1666eb9eef4239a716dde870d401ce4e1cbe1
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-04-12 12:56:33 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 // Array of API URLs to fetch data from
 const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/1",
@@ -13,3 +15,31 @@ const apiUrls = [
 ];
 
 // You can write your code here
+function fetchData(url) {
+  return fetch(url).then(response => response.json());
+}
+function testPromiseAll() {
+  const start = Date.now();
+  Promise.all(apiUrls.map(fetchData)).then(results => {
+    const end = Date.now();
+    const timeTaken = end - start;
+    document.getElementById('output-all').innerText = `${timeTaken} ms`;
+    console.log(results);
+  })
+  .catch(error => console.error(error));
+}
+
+function testPromiseAny() {
+  const start = Date.now();
+  Promise.any(urls.map(fetchData))
+    .then(result => {
+      const end = Date.now();
+      const timeTaken = end - start;
+      document.getElementById('output-any').innerText = `${timeTaken} ms`;
+      console.log(result);
+    })
+    .catch(error => console.error(error));
+}
+
+testPromiseAll();
+testPromiseAny();
